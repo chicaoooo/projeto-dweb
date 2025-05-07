73f84302b682754c92dbf4f9ff978aa4174af3e2
Francisco Margarido <160274340+chicaoooo@users.noreply.github.com>
2025-05-07 17:35:24 +0100

    Commit inicial com algumas alteraçoes no footer e texto em portugues

---
+++ DIEARD/DIEARD/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DIEARD.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
